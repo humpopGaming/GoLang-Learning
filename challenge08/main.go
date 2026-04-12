e9dbba93f5647578410bef1c6f6d078feda8b9d0
Tony Simpson <humpopstudio@outlook.com>
2026-04-12 10:13:21 +0100

    Add detailed notes on pointers to structs in main.go

--- challenge08/main.go
+++ challenge08/main.go
@@ -1,1 +1,11 @@
 package main
+
+/*
+Notes:
+# Pointers to structs
+To access the field X of a struct when we have the struct pointer p
+we could write (*p).X. However, that notation is cumbersome,
+so the language permits us instead to write just p.X,
+without the explicit dereference.
+-> need to understanding the dereferencing terminology and why (*p).X is explict dereferencing
+*/
